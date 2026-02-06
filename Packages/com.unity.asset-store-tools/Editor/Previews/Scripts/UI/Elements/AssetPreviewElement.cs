54eebb11c9cc01f430e92581bc5d0cd29ad737d8
Diogo Teixeira <diogo@amplify.pt>
2026-02-06 15:52:03 +0000

    Added asset store tools

---
+++ Packages/com.unity.asset-store-tools/Editor/Previews/Scripts/UI/Elements/AssetPreviewElement.cs
@@ -0,0 +1,71 @@
+using AssetStoreTools.Previews.UI.Data;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace AssetStoreTools.Previews.UI.Elements
+{
+    internal class AssetPreviewElement : VisualElement
+    {
+        // Data
+        private IAssetPreview _assetPreview;
+
+        // UI
+        private Image _image;
+        private Label _label;
+
+        public AssetPreviewElement()
+        {
+            AddToClassList("preview-list-image");
+
+            Create();
+
+            RegisterCallback<MouseDownEvent>(OnImageClicked);
+        }
+
+        private void Create()
+        {
+            CreateFiller();
+            CreateImage();
+            CreateLabel();
+        }
+
+        private void CreateImage()
+        {
+            _image = new Image();
+            Add(_image);
+        }
+
+        private void CreateFiller()
+        {
+            var filler = new VisualElement() { name = "Filler" };
+            Add(filler);
+        }
+
+        private void CreateLabel()
+        {
+            _label = new Label();
+            Add(_label);
+        }
+
+        private void SetImage(Texture2D texture)
+        {
+            _image.style.width = texture.width < 128 ? texture.width : 128;
+            _image.style.height = texture.height < 128 ? texture.height : 128;
+            _image.style.backgroundImage = texture;
+        }
+
+        private void OnImageClicked(MouseDownEvent _)
+        {
+            EditorGUIUtility.PingObject(_assetPreview.Asset);
+        }
+
+        public void SetSource(IAssetPreview assetPreview)
+        {
+            _assetPreview = assetPreview;
+            _assetPreview.LoadImage(SetImage);
+            _label.text = _assetPreview.Asset.name;
+            tooltip = _assetPreview.GetAssetPath();
+        }
+    }
+}
